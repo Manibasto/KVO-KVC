6d2bf3abb6fc80ae7b042385bb1302103c5f9bed
manikandanaitech <manikandan.s@aitechindia.com>
2019-06-25 12:32:22 +0530

    Initial Commit

---
+++ KVO&KVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVO&KVC
+//
+//  Created by Anil Kumar on 25/06/19.
+//  Copyright © 2019 AIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
